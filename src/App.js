66087f0e7d25f297f0d63d8458aff1e7d38a627e
Vadim Kiryanov <vadim.kiryanov@mail.ru>
2022-10-07 21:00:59 +0300

    Gallery BETA

--- src/App.js
+++ src/App.js
@@ -1,72 +1,86 @@
-import React, { useEffect, useRef, useState } from 'react';
-import { Block } from './Block';
+import React, { useEffect, useState } from 'react';
+import Collection from './components/Collection';
 import './index.scss';
 
+
+const categories = [
+  { "name": "Все" },
+  { "name": "Море" },
+  { "name": "Горы" },
+  { "name": "Архитектура" },
+  { "name": "Города" }
+];
+
 function App() {
-  // Выбор валюты
-  const [fromCurrency, setFromCurrency] = useState("USD");
-  const [toCurrency, setToCurrency] = useState("RUB");
-
-  // Вводимые данные в инпут
-  const [fromPrice, setFromPrice] = useState(0);
-  const [toPrice, setToPrice] = useState(0);
-
-  // Взятие данных с бэка
-  // const [rates, setRates] = useState({});
-  const ratesRef = useRef({});
-
+  // Категории фото
+  const [categoryId, setCategoryId] = useState(0);
+  // Пагинация
+  const [page, setPage] = useState(1);
+  // Состояние загрузки категорий
+  const [isLoading, setIsLoading] = useState(true);
+  // Контролируемый input
+  const [searchValue, setSearchValue] = useState('');
   // Запрос к БД
+  const [gallery, setGallery] = useState([]);
   useEffect(() => {
-    fetch("https://cdn.cur.su/api/latest.json")
+    setIsLoading(true) // !!!
+
+    const category = categoryId ? `category=${categoryId}` : "";
+
+    fetch(`https://633e73820dbc3309f3b5d032.mockapi.io/photo_collections?page=${page}&limit=3&${category}`) // Повторный запрос к БД для обновления коллекции
       .then(res => res.json())
       .then(json => {
-        // setRates(json.rates)
-        ratesRef.current = json.rates;
-        onChangeFromPrice(1) // Установка значения при получении БД
+        setGallery(json)
       }).catch(err => {
-        console.log(err);
-        alert("Ошибка при получении данных валют")
-      })
-  }, []);
-
-  // Конвертация валюты
-  const onChangeFromPrice = (value) => {
-    const price = value / ratesRef.current[fromCurrency];
-    const result = price * ratesRef.current[toCurrency];
-    setToPrice(result.toFixed(3))
-    setFromPrice(value)
-  };
-
-  const onChangeToPrice = (value) => {
-    const result = (ratesRef.current[fromCurrency] / ratesRef.current[toCurrency]) * value;
-    setFromPrice(result.toFixed(3))
-    setToPrice(value)
-  };
-
-  // Динамическая конвертация валют
-  useEffect(() => {
-    onChangeFromPrice(fromPrice)
-  }, [fromCurrency]);
-
-  useEffect(() => {
-    onChangeToPrice(toPrice)
-  }, [toCurrency]);
+        console.log(err)
+        alert("Ошибка при получении галереи")
+      }).finally(() => setIsLoading(false)) // !!!
+  }, [categoryId, page]);
 
 
   return (
     <div className="App">
-      <Block
-        value={fromPrice}
-        currency={fromCurrency}
-        onChangeCurrency={setFromCurrency}
-        onChangeValue={onChangeFromPrice}
-      />
-      <Block
-        value={toPrice}
-        currency={toCurrency}
-        onChangeCurrency={setToCurrency}
-        onChangeValue={onChangeToPrice}
-      />
+      <h1>Моя коллекция фотографий</h1>
+      <div className="top">
+        <ul className="tags">
+          {categories.map((elem, index) => (
+            <li
+              onClick={() => setCategoryId(index)}
+              className={categoryId === index ? "active" : ""}
+              key={index}>{elem.name}
+            </li>
+          ))}
+        </ul>
+        <input
+          value={searchValue}
+          onChange={e => setSearchValue(e.target.value)}
+          className="search-input"
+          placeholder="Поиск по названию" />
+      </div>
+
+      <div className="content">
+        {isLoading ? (<h2>Идет загрузка...</h2>
+        ) : (
+          gallery.filter(elem => elem.name.toLowerCase().includes(searchValue.toLowerCase()))
+            .map((elem, index) => (
+              <Collection
+                key={index}
+                name={elem.name}
+                images={elem.photos}
+              />
+            ))
+        )}
+      </div>
+
+      {/* Пагинация */}
+      <ul className="pagination">
+        {[...Array(5)].map((_, index) => (
+          <li
+            onClick={() => setPage(index + 1)}
+            className={page === index + 1 ? "active" : ""}>{index + 1}</li>
+        ))}
+      </ul>
+
     </div>
   );
 }

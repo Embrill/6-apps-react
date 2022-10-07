432de8d17761375bedbd22313d754a2d8c6f0a6a
Vadim Kiryanov <vadim.kiryanov@mail.ru>
2022-10-07 20:36:57 +0300

    Counter BETA

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+import './index.scss';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const countPlus = () => {
+    setCount(prev => prev + 1)
+  };
+
+  const countMinus = () => {
+    setCount(prev => prev - 1)
+  };
+
+  return (
+    <div className="App">
+      <div>
+        <h2>Счетчик:</h2>
+        <h1>{count}</h1>
+        <button className="minus" onClick={countMinus}>- Минус</button>
+        <button className="plus" onClick={countPlus}>Плюс +</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
